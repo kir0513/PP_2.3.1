98b971ffabbbcb6db3f86167e72c6965e44c749d
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 09:01:40 +0300

    Создадим приложение, которое будет запускаться и управляться с помощью сервера Tomcat. IDE поможет работать с ним без сложных консольных команд, что сильно облегчит разработку. В этом приложении мы используем только зависимость spring-webmvc. И данном случае webmvc-фреймворк будет работать самостоятельно, так как любой фреймворк Спринга содержит в себе spring-core. В приложении появился новый пакет controller, в котором содержатся классы, держащие группы сервлетов - контроллеры. Данная технология позволит нам избежать создания большого количества сервлетов. Контроллеры содержат методы, которые маппятся на разные url и http-методы, заменяя собой класс сервлета с методами doGet, doPost и т.д.

---
+++ src/main/java/web/config/model/Car.java
@@ -0,0 +1,47 @@
+package web.config.model;
+
+public class Car {
+    private String model;
+    private String color;
+    private int power;
+    public Car(){}
+
+    public Car(String model, String color, int power) {
+        this.model = model;
+        this.color = color;
+        this.power = power;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", power=" + power +
+                '}';
+    }
+}
