203d516abcaebc6b919e7bbf7b296bf796dee286
kir0513 <kir0513@yandex.ru>
2023-05-02 13:04:58 +0300

    Добавил hash equals

--- src/main/java/web/config/model/Car.java
+++ src/main/java/web/config/model/Car.java
@@ -1,5 +1,7 @@
 package web.config.model;
 
+import java.util.Objects;
+
 public class Car {
     private String model;
     private String color;
@@ -43,5 +45,19 @@ public class Car {
                 ", color='" + color + '\'' +
                 ", power=" + power +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return power == car.power && model.equals(car.model) && color.equals(car.color);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(model, color, power);
     }
 }
--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService1;
-import web.service.CarService2;
 
 import java.util.Optional;
 
@@ -14,17 +13,16 @@ public class CarsController {
 
     private final CarService1 carService1;
 
-    private final CarService2 carService2;
 
-    public CarsController(CarService1 carService1, CarService2 carService2) {
+    public CarsController(CarService1 carService1) {
         this.carService1 = carService1;
-        this.carService2 = carService2;
+
     }
 
 
     @GetMapping(value = "/cars")
     public String showCars(@RequestParam(required = false, value = "count") Optional<Integer> count, Model model) {
-        if (count.isPresent() && count.get() >=0 && count.get() <= 5) {
+        if (count.isPresent() && count.get() >= 0 && count.get() <= 5) {
             model.addAttribute("cars", carService1.getPartOfListCar(count.get()));
         } else {
             model.addAttribute("cars", carService1.getAllCars());
